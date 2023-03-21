b3825aa39632bf4e2e32d4ac4ffbbc8153bcd9dd
Victor <vsifah@gmail.com>
2023-03-21 20:31:52 +0100

    editing Dockerfile

--- api/DbConnect.php
+++ api/DbConnect.php
@@ -3,14 +3,14 @@
 	* Database Connection
 	*/
 	class DbConnect {
-		private $server = 'mysql://root:root@mysql:3306/react_crud';
+		private $server = 'mysql://root:root@mysql:33060/react_crud';
 		private $dbname = 'react_crud';
 		private $user = 'root';
 		private $pass = 'root';
 
 		public function connect() {
 			try {
-				$conn = new PDO('mysql://root:root@mysql:3306/react_crud');
+				$conn = new PDO('mysql://root:root@mysql:33060/react_crud');
 				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				return $conn;
 			} catch (\Exception $e) {
